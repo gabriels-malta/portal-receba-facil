512fd28387c4d91cbd2d61522473c39bc46044ff
Gabriel Malta <47032867+gabriels-malta@users.noreply.github.com>
2020-06-30 19:56:29 -0300

    Showroom (#7)
    * nova listagem de encomendas para ponto de retirada
    
    * ajuste na visualização do expediente
    
    * add encomendaServiceTest
    
    * layout + crud de expediente
    
    * correção de testes
    
    * override repository base

--- src/RecebaFacil.Repository/RepositoryUsuario.cs
+++ src/RecebaFacil.Repository/RepositoryUsuario.cs
@@ -1,6 +1,10 @@
-﻿using RecebaFacil.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RecebaFacil.Domain.Entities;
 using RecebaFacil.Repository.ContextConfig;
 using RecebaFacil.Repository.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RecebaFacil.Repository
 {
@@ -8,7 +12,15 @@ namespace RecebaFacil.Repository
     {
         public RepositoryUsuario(RFContext context)
             : base(context)
+        { }
+
+        public async override ValueTask<Usuario> ObterPorId(Guid id, CancellationToken cancellationToken = default)
         {
+            return await _context.Usuario
+                .Include(x => x.Grupo)
+                .Include(x => x.Empresa)
+                .FirstAsync(x => x.Id == id, cancellationToken)
+                .ConfigureAwait(false);
         }
     }
 }
