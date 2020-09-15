25f8535890942cb4bfc570a77266fa01a07e5537
Gabriel Malta <47032867+gabriels-malta@users.noreply.github.com>
2020-09-15 19:03:34 -0300

    Sonar qa (#17)
    * Add ProjectGuid to projects
    
    * remove sonar.lint & other localhost configuration
    
    * Turn "Microrregioes" in a method

--- src/RecebaFacil.Domain/Core/Constants/CacheKeys.cs
+++ src/RecebaFacil.Domain/Core/Constants/CacheKeys.cs
@@ -2,7 +2,7 @@
 {
     public static class CacheKeys
     {
-        public static string Microrregioes = "MICRORREGIOES";
+        public static string Microrregioes => "MICRORREGIOES";
 
         public static string UsuarioLogado => "UsuarioLogado";
     }
